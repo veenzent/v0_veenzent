db7536bbe3cce098a05bce399d5dd2f9a5ef9414
veenzent <odumevincent19@gmail.com>
2026-06-15 16:20:03 +0100

    fix: Iconify-icon type error

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,12 @@
 declare namespace JSX {
   interface IntrinsicElements {
-    'iconify-icon': any;
+    // 'iconify-icon': any;
+    'iconify-icon': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+          icon?: string;
+          style?: string | number;
+          width?: string | number;
+          height?: string | number;
+          color?: string;
+        };
   }
 }
